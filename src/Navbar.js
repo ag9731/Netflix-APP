a8013ab56de2322c3e7686be14be3aa73dead07e
Akash <goravanakollakash@gmail.com>
2022-12-15 11:06:21 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Nav.css";
+
+function Navbar() {
+  return (
+    <div className='nav'>
+      <img className='nav_logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt="Netflix Logo"/>
+    </div>
+  )
+}
+
+export default Navbar
